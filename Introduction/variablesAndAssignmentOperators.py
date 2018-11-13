4b717192388a9a3d718881bc7067f7fb3bfd2c1a
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-13 21:25:58 +0200

    Variables - multiple values assignment

---
+++ Introduction/variablesAndAssignmentOperators.py
@@ -0,0 +1,9 @@
+def varValuesAssignmentExample():
+    x = 2
+    y = 3
+    z = 5
+    i, j, k = 7, 8, 9
+    print(x, y, z, i, j, k)
+
+if __name__ == '__main__':
+    varValuesAssignmentExample()
