c718f10435f7e981cc6f52972129aa4db6deb40f
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-13 20:54:34 +0200

    Data Types and operations - part 1

---
+++ Introduction/dataTypesAndOperators.py
@@ -0,0 +1,10 @@
+def arithmetic_operations():
+    print(1 + 2)
+    print(3 * 5)
+    print(8 - 7)
+    print(126 / 5)
+    print(123 % 4)
+    print(123 // 4)
+
+if __name__ == '__main__':
+    arithmetic_operations()    
