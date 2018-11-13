29a6781aa084fdceac8272c28cc5f89889d0c97c
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-13 21:05:33 +0200

    Data types - Average Electricity Bill task

--- Introduction/dataTypesAndOperators.py
+++ Introduction/dataTypesAndOperators.py
@@ -6,6 +6,19 @@ def arithmetic_operations():
     print(123 % 4)
     print(123 // 4)
     print(3 ** 4)
+    print('End of arithmetic operators example')
+
+'''
+My electricity bills for the last three months have been $23, $32 and $64. 
+What is the average monthly electricity bill over the three month period? 
+Write an expression to calculate the mean, and use print() to view the result.
+'''
+def avetageElectricityBill():
+    print('Average electricity bill task')
+    print((23 + 32 + 64) /3)
+    print('End of task')
+
 
 if __name__ == '__main__':
-    arithmetic_operations()    
+    arithmetic_operations()   
+    avetageElectricityBill() 
