0ba312ce68b465be3db537229c784c7a8744a782
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-13 21:17:07 +0200

    Data types - tiles calculation

--- Introduction/dataTypesAndOperators.py
+++ Introduction/dataTypesAndOperators.py
@@ -18,7 +18,20 @@ def avetageElectricityBill():
     print((23 + 32 + 64) /3)
     print('End of task')
 
+'''
+In this quiz you're going to do some calculations for a tiler. 
+Two parts of a floor need tiling. 
+One part is 9 tiles wide by 7 tiles long, the other is 5 tiles wide by 7 tiles long. 
+Tiles come in packages of 6.
+How many tiles are needed?
+You buy 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
+'''
+def calculate():
+    neededTiles = (9 * 7) + (7 * 5)
+    print(neededTiles)
+    print(102 - neededTiles)
 
 if __name__ == '__main__':
     arithmetic_operations()   
     avetageElectricityBill() 
+    calculate()
